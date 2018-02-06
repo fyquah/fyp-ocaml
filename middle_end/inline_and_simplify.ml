e902886b03d8ff38730ea4fb461492ed0297f49a
Fu Yong Quah <fyq14@imperial.ac.uk>
2018-02-06 11:47:15 +0000

    FYP: Revert unintended on FloatArray checks.

--- middle_end/inline_and_simplify.ml
+++ middle_end/inline_and_simplify.ml
@@ -1046,21 +1046,19 @@ and simplify_named env r (tree : Flambda.named) : Flambda.named * R.t =
           Location.prerr_warning (Debuginfo.to_location dbg)
             Warnings.Assignment_to_non_mutable_value
         end;
-        let kind = match A.descr block_approx, A.descr value_approx with
-          | (Value_float_array _, _)
-          | (_, Value_float _) ->
-            begin match kind with
-            | Pfloatarray | Pgenarray
-            | Paddrarray | Pintarray -> ()
-(* For the purpose of data mining, should be able to pretend nothing happened. *)
-(*
+        let kind =
+          let check () =
+            match kind with
+            | Pfloatarray | Pgenarray -> ()
+            | Paddrarray | Pintarray ->
               Misc.fatal_errorf "Assignment of a float to a specialised \
                                  non-float array: %a"
                 Flambda.print_named tree
-*)
-            end;
-            Lambda.Pfloatarray
-            (* CR pchambart: This should be accounted by the benefit *)
+          in
+          match A.descr block_approx, A.descr value_approx with
+          | (Value_float_array _, _) -> check(); Lambda.Pfloatarray
+          | (_, Value_float _) when Config.flat_float_array ->
+            check (); Lambda.Pfloatarray
           | _ ->
             kind
         in
